7a67fab9f2951ba9ba99ca9dee794305cee3ea87
MatheusMarinelli <matheushlmarinelli@gmail.com>
2024-11-22 16:20:17 -0300

    implementing circuit breaker on service

--- spring-cloud-open-feign/feign-client/src/main/java/com/spring/cloud/controller/ClientController.java
+++ spring-cloud-open-feign/feign-client/src/main/java/com/spring/cloud/controller/ClientController.java
@@ -1,10 +1,12 @@
 package com.spring.cloud.controller;
 
+import com.spring.cloud.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import com.spring.cloud.service.ClientService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/client")
--- spring-cloud-open-feign/feign-client/src/main/java/com/spring/cloud/service/ClientService.java
+++ spring-cloud-open-feign/feign-client/src/main/java/com/spring/cloud/service/ClientService.java
@@ -1,18 +1,25 @@
 package com.spring.cloud.service;
 
+import com.spring.cloud.client.CalculatorClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.spring.cloud.client.CalculatorClient;
-
 @Service
 public class ClientService {
-	
-	// inject the interface and use it
-	@Autowired
-	private CalculatorClient client;
-	
-	public Double sum(Double a, Double b) {
-		return client.sum(a, b);
-	}
+
+    // inject the interface and use it
+    @Autowired
+    private CalculatorClient client;
+
+    @CircuitBreaker(name = "sum", fallbackMethod = "sumFallback") // fallbackMethod must have the same return of its origin method
+    public Double sum(Double a, Double b) {
+        return client.sum(a, b);
+    }
+
+
+    public Double sumFallback(Double a, Double b, Exception e) {
+        return 0.0;
+    }
+
 }
