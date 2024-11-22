381c6c72721b13382a71a8041f6d916e07509195
MatheusMarinelli <matheushlmarinelli@gmail.com>
2024-11-22 16:20:55 -0300

    update spring version to 3.3.5

--- spring-cloud-open-feign/calculator-service/src/main/java/com/spring/cloud/controller/MathController.java
+++ spring-cloud-open-feign/calculator-service/src/main/java/com/spring/cloud/controller/MathController.java
@@ -1,7 +1,10 @@
 package com.spring.cloud.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/math")
@@ -10,6 +13,7 @@ public class MathController {
 	@GetMapping("/sum")
 	public ResponseEntity<Double> sum(@RequestParam(value = "firstNumber", defaultValue = "0") Double firstNumber,
 									  @RequestParam(value = "secondNumber", defaultValue = "0") Double secondNumber) {
+
 		return ResponseEntity.ok(firstNumber + secondNumber);
 	}
 
